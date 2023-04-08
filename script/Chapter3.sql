0d6e2ca41058a23d0897f0cd8468ae13ae558bfc
HORRYZONE <danishfitri131@gmail.com>
2023-04-09 04:08:11 +0800

    Update chapter3✨
    got bug on limit row selected 🚫

--- script/Chapter3.sql
+++ script/Chapter3.sql
@@ -31,5 +31,46 @@ FROM doctor;
 SELECT doc_name, annual_bonus AS "Bonus Upgrade" 
 FROM doctor;
 
+--concantenational operator ( || ) use to bind 2 variable in 1 space
 SELECT doc_name||area AS "Doctor" 
 FROM doctor;
+
+--Each character string is output once for each row return
+SELECT doc_name || ' is from ' || area AS "DOCTOR" 
+FROM doctor;
+
+--return unique values for the specified column. how many type is there
+SELECT DISTINCT area 
+FROM doctor; 
+
+--Limiting the Rows Selected
+SELECT *| { [DISTINCT] column | expression 
+[alias],...} 
+FROM table 
+WHERE condition(s);
+
+-- find doctor who charge per appoint is >= 40
+SELECT doc_name 
+FROM doctor 
+WHERE chgperappt >= 40;
+
+-- find doc which annual between 2 values
+SELECT doc_name, annual_bonus
+FROM doctor 
+WHERE annual_bonus BETWEEN 2000 AND 
+4000;
+
+--find doc_name that start with 'J'
+SELECT * 
+FROM doctor 
+WHERE doc_name LIKE 'J%';
+
+--use concan operator & AS cmd & LIKE cmd
+SELECT lname || ', ' || fname AS "FULL NAME", phone 
+FROM customer 
+WHERE phone LIKE '549-67__' ;
+
+-- use NOT operator to find condition needed
+SELECT doc_id, doc_name, annual_bonus 
+FROM doctor 
+WHERE annual_bonus IS NOT NULL; 
